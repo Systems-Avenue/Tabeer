b285a94ea7fd2364c4ee57419b1bb59a2474cc62
Qaiser Hussain <qaiserui11@gmail.com>
2022-06-29 15:11:03 +0500

    Add project files.

---
+++ Tabeer_PP_Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tabeer_PP_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
